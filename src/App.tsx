b95968f29c14322ae31883e377acca0f2355f670
Joshua Yustana <joshiosimoe@gmail.com>
2025-04-04 21:13:13 -0500

    Josh First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import MapPage from './pages/MapPage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -61,8 +62,11 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route exact path="/MapPage">
+            <MapPage/>
+          </Route>
           <Route exact path="/">
-            <Redirect to="/tab1" />
+            <Redirect to="AuthPage" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
@@ -77,6 +81,10 @@ const App: React.FC = () => (
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
             <IonLabel>Tab 3</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="mappage" href="/mappage">
+            <IonIcon aria-hidden="true" icon={square} />
+            <IonLabel>Map Page</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
